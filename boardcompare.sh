da4b90426cf924e75ffe81444c729e059a13ea06
Kieran <kierancluchey@gmail.com>
2016-11-29 22:20:23 -0500

    fix boardcompare errors... it totally wasn't working on first upload

--- boardcompare.sh
+++ boardcompare.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/expect
-# dumb way of defining chess board positions... indexed 0-63
-set boardref [split "a8 b8 c8 d8 e8 f8 g8 h8 a7 b7 c7 d7 e7 f7 g7 h7 a6 b6 c6 d6 e6 f6 g6 h6 a5 b5 c5 d5 e5 f5 g5 h5 a5 b5 c5 d5 e5 f5 g5 h5 a4 b4 c4 d4 e4 f4 g4 h4 a3 b3 c3 d3 e3 f3 g3 h3 a2 b2 c2 d2 e2 f2 g2 h2 a1 b1 c1 d1 e1 f1 g1 h1"]
 
 # define function that allows conversion of board states into
 # an exact algebraic notation move
 proc convertMove {firstboard secondboard} {
+
+# dumb way of defining chess board positions... indexed 0-63
+set boardref [split "a8 b8 c8 d8 e8 f8 g8 h8 a7 b7 c7 d7 e7 f7 g7 h7 a6 b6 c6 d6 e6 f6 g6 h6 a5 b5 c5 d5 e5 f5 g5 h5 a5 b5 c5 d5 e5 f5 g5 h5 a4 b4 c4 d4 e4 f4 g4 h4 a3 b3 c3 d3 e3 f3 g3 h3 a2 b2 c2 d2 e2 f2 g2 h2 a1 b1 c1 d1 e1 f1 g1 h1"]
 
 #	these variables will never be set to >63 so if either
 #	is still 65 in loop then we aren't done searchin'
@@ -14,10 +15,10 @@ proc convertMove {firstboard secondboard} {
 
 #	loop that controls board comparison, when both positions
 #	are not== 65 (i.e. determined) then loop terminates
-	while {$initPos == 65 || $finaPos == 65} {
+	while {$i < 64} {
 
 #		ignore all positions that are the same in both boards
-		if { [lindex $firstboard $i] == [lindex $secondboard $i] } {
+		if {[string equal [lindex $firstboard $i] [lindex $secondboard $i]]} {
 			incr i 1
 			continue
 
@@ -27,7 +28,7 @@ proc convertMove {firstboard secondboard} {
 #			checks if after latest move, a board square
 #			has a ".", implying this is the position from
 #			which the piece has moved
-			if {[string match \. [lindex $secondboard $i]]} {
+			if {[string equal "." [lindex $secondboard $i]]} {
 				set initPos $i
 				incr i 1
 				continue
@@ -41,17 +42,26 @@ proc convertMove {firstboard secondboard} {
 			}
 		}
 	}
-
+#puts "$initPos"
+#puts "$finaPos"
 #	we could return #exactAlgMove here but there might be a promotion of a pawn
 	set exactAlgMove [string cat [lindex $boardref $initPos] [lindex $boardref $finaPos]]
 	set promotion ""
 
 #	checks for promotion of pawn by seeing if initial position had a pawn and
 #	final position does not have a pawn
-	if { [string match [P p] [lindex $firstboard $initPos]] && ![string match [P p] [lindex $secondboard $finaPos]] } {
+	if { [string equal -nocase "P" [lindex $firstboard $initPos]] && ![string equal -nocase "P" [lindex $secondboard $finaPos]] } {
 		set promotion [lindex $secondboard $finaPos]
 	}
 
 #	returns either concatenation of $exactAlMove and an empty string or the promotion
 	return [string cat $exactAlgMove $promotion]
 }
+
+# test
+#set b1 [split "p . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ." ]
+#set b2 [split ". q . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ." ]
+#puts [convertMove $b1 $b2]
+
+
+
